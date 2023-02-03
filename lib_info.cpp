52ccaaf6317ba433377db17fc678ec08a14615b0
Emily L Vo <evo3@hydra11.eecs.utk.edu>
2023-02-03 09:11:37 -0500

    updated version

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,26 +1,33 @@
-// Emily Vo and Dhwani Patel
+// Emily Vo & Dhwani Patel
+// Project 1
+// This program reads in data about songs, artists, and albums from a file, and stores the information in a series of maps
+// CS 302
+// 2/2/23
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <vector>
 #include <string>
 #include <map>
-#include <algorithm>
 #include <string.h>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 
+/* define the structs for Song, Album, and Artist */
 struct Song
 {
     string title;
-    string time; // could also be a string
+    string time;
+    int track;
 };
 
 struct Album
 {
-    map<string, Song> songs;
+    map<int, Song> songs;
     string name;
     int time = 0;
-    int nsongs; // optional variable but makes it easier
+    int nsongs;
 };
 
 struct Artist
@@ -31,80 +38,109 @@ struct Artist
     int nsongs;
 };
 
-
-void time(string &line)
-{
-    int min = 0, sec = 0;
-    for (int i = 0; i < line.size(); i++)
-    {
-        min += stoi(line[1].substr(0, line[1].find(":")));
-        sec += stoi(line[1].substr(line[1].find(":") + 1));
-
-        if (sec >= 60)
-        {
-            min += 1;
-            sec -= 60;
-        }
-        cout << setfill('0') << setw(2) << min << " " << sec << endl << endl;
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    string line, nTitle, nArtist, nAlbum, nGenre, nTrack;
-    map <string, Artist> artists;
-	map <string, Artist>::iterator artist_it;
-	map <int, Song>::iterator song_it;
-	map <string, Album>::iterator album_it;
+    string line, nTitle, nTime, nArtist, nAlbum, nGenre;
+    int nTrack;
+    map<string, Artist> artists;
+    // iterators for the artists, albums, and songs maps
+    map<string, Artist>::iterator artist_it;
+    map<int, Song>::iterator song_it;
+    map<string, Album>::iterator album_it;
 
+    // variables for the current song, artist, and album
+    Song cSong;
+    Artist cArtist;
+    Album cAlbum;
 
+    // open the file from the command line argument
+    ifstream fin;
+    fin.open(argv[1]);
 
-    while(getline(cin, line))
+    while (getline(fin, line))
     {
-//        replace(line.begin(), line.end(), '_', ' ');
-
-		//reading in the input as strings
+        // read in the input as strings
         stringstream ss(line);
-		replace(line.begin(), line.end(), '_', ' ');
-
         ss >> nTitle >> nTime >> nArtist >> nAlbum >> nGenre >> nTrack;
-		
-		Song cSong;
 
-		cSong.title = nTitle;
-		cSong.time = nTime;
+        replace(nTitle.begin(), nTitle.end(), '_', ' ');
+        replace(nArtist.begin(), nArtist.end(), '_', ' ');
+        replace(nAlbum.begin(), nAlbum.end(), '_', ' ');
+        replace(nGenre.begin(), nGenre.end(), '_', ' ');
 
-		//check if the artist exists in the map 'artists'
-		
-		artist_it = artist.find(nArtist);
-		if(artist_it == artist.end()){
-			
-			Artist cArtist;
+        // assign the values to the current song; store the info in teh cSong struct
+        cSong.title = nTitle;
+        cSong.time = nTime;
+        cSong.track = nTrack;
 
-			cArtist.name = nArtist;
-			cArtist.time = 0;
+        // check if the artist exists in the map 'artists'
+        artist_it = artists.find(nArtist);
 
-			artist.insert(pair<cArtist.name, cArtist>);
-			
-		}
-		else{
-			album_it = album.find(nAlbum);
-	        if(album_it == album.end()){
+        // if the artist does not exist, add them to the map; create a new artist
+        if (artist_it == artists.end())
+        {
+            cArtist.name = nArtist;
+            cArtist.time = 0;
+            cArtist.nsongs = 0;
 
-		        Album cAlbum;
+            artists.insert(make_pair(nArtist, cArtist));
+            artist_it = artists.find(nArtist);
+        }
 
-			    cAlbum.name = nAlbum;
-				cAlbum.time = 0;
-				album.insert(pair<cAlbum.name, cAlbum>);
+        // increase the # of songs for the artist
+        artist_it->second.nsongs += 1;
+        // check if the album exists in the artist's albums map
+        album_it = artist_it->second.albums.find(nAlbum);
 
-			}
+        // if not, create a new album
+        if (album_it == artist_it->second.albums.end())
+        {
+            cAlbum.name = nAlbum;
+            cAlbum.time = 0;
+            cAlbum.nsongs = 0;
+            artist_it->second.albums.insert(make_pair(nAlbum, cAlbum));
+            album_it = artist_it->second.albums.find(nAlbum);
+        }
 
-		}
-		
-		song.insert(pair<cSong.time, cSong>);
+        // increase the number of songs for the album
+        album_it->second.nsongs += 1;
+        // keep inserting songs
+        album_it->second.songs.insert(make_pair(nTrack, cSong));
 
+        /* calculate the total time for the artist and album */
+        int nMinutes = 0;
+        int nSeconds = 0;
+        int colonIndex = nTime.find(":");
 
+        // string to int conversion
+        nMinutes = stoi(nTime.substr(0, colonIndex));
+        nSeconds = stoi(nTime.substr(colonIndex + 1));
+
+        int totalSeconds = nMinutes * 60 + nSeconds;
+
+        // add total time to artist and album times
+        artist_it->second.time += totalSeconds;
+        album_it->second.time += totalSeconds;
     }
 
+    /* sort the map of songs within each album under each artist; print out the results */
+    for (artist_it = artists.begin(); artist_it != artists.end(); artist_it++)
+    {
+        cout << artist_it->second.name << ": " << artist_it->second.nsongs << ", " << artist_it->second.time / 60 << ":" << setfill('0') << setw(2) << artist_it->second.time % 60 << endl;
+
+        for (album_it = artist_it->second.albums.begin(); album_it != artist_it->second.albums.end(); album_it++)
+        {
+
+            cout << "        " << album_it->second.name << ": " << album_it->second.nsongs << ", " << album_it->second.time / 60 << ":" << setfill('0') << setw(2) << album_it->second.time % 60 << endl;
+
+            for (song_it = album_it->second.songs.begin(); song_it != album_it->second.songs.end(); song_it++)
+            {
+
+                cout << "                " << song_it->second.track << ". " << song_it->second.title << ": " << song_it->second.time << endl;
+            }
+        }
+    }
+    fin.close();
     return 0;
 }
+
